5e8dad6b96d6b48f5484cc5abfd76d0dd747f20e
devkhedr <mohamed.khedr1714@gmail.com>
2023-03-19 02:42:16 +0200

    update urls

--- django_rest_auth/urls.py
+++ django_rest_auth/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.debug import default_urlconf
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -14,7 +15,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema_swagger_ui"),
+    path("", default_urlconf),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema_swagger_ui"),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("authentication/", include("authentication.urls")),
